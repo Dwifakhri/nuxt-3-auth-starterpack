05a61159c5c752502cd83613fcfb40e43eedb521
fakhri <fakhri.chusaini01@gmail.com>
2024-12-13 21:14:38 +0700

    fix robot allow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,7 @@ export default defineNuxtConfig({
     groups: [
       {
         userAgent: "*",
-        disallow: ["/"],
+        disallow: ["/", "/login"],
         comment: "Disabled specific URL",
       },
     ],
